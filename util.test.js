ecdf1faefbe0132c0922e09093acc04b8e593dce
Song Cho <deeplearner507@gmail.com>
2019-06-09 15:55:44 +0900

    integration test

--- util.test.js
+++ util.test.js
@@ -1,10 +1,17 @@
-const { generateText } = require('./util');
+const { generateText, checkAndGenerate } = require('./util');
 
 // import is not a syntax natively supported by jest
 // import {generateText} from  './util';
 
 //globally available function provided by jest
+//Unit test
 test('should output name and age', ()=>{
     const text = generateText('Song', 88);
     expect (text).toBe('Song (88 years old)');
 });
+
+//Integration test
+test('should generate a valid text output', () => {
+    const text = checkAndGenerate('Yoda',1004);
+    expect(text).toBe('Yoda (1004 years old)')
+  });
