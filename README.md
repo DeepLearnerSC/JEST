2f66a8f48414a85b7c8e6e35c226f8b6d56a4018
Song Cho <deeplearner507@gmail.com>
2020-01-05 15:08:32 +0900

    upadate reademe.md

--- README.md
+++ README.md
@@ -1,8 +1,11 @@
+# Introduction
+99% of the codes and tutorials are created by Maximilian Schwarzmüller.
+This repo is for the personal use only or you can simply clone repo and pracitice JEST!
+ 
 # Reference
+resource: https://academind.com/learn/javascript/javascript-testing-introduction
 
-JavaScript testing (unit tests, integration tests, e2e tests) introduction repo - belongs to this tutorial: 
-
-https://academind.com/learn/javascript/javascript-testing-introduction
+Part 1. JavaScript testing (unit tests, integration tests, e2e tests) 
 
 # Description
 to be customized and updated later T.T
@@ -20,11 +23,6 @@ $ git clone git@github.com:DeepLearnerSC/JEST.git
 $ npm install
 ```
 
-```bash
-$ npm install --save-dev jest
-```
-
-
 for testing e2e
 puppeteer is headless version of chrome browser which interects the dom.
 ```bash
@@ -32,4 +30,22 @@ $ npm install --save-dev puppeteer
 ```
 
 
-part 2.
+Keynote:
+
+in the util.test.js file, you need to configure your path for index.html
+
+What you will need to do is the following steps.
+1. create 'path.js' file.
+2. and add the path codes.  (use 'pwd' command)
+example) 
+```bash
+exports.path = 'file:///Users/USERNAME/development/JEST/index.html';
+```
+```bash
+$ npm start
+```
+
+part 2. Mocking Async Code
+
+This tutorial is based on academind.com: 
+https://www.youtube.com/watch?v=4Fl5GH4eYZ8
--- util.test.js
+++ util.test.js
@@ -1,38 +1,40 @@
-const puppeteer = require('puppeteer');
+const puppeteer = require("puppeteer");
 const { path } = require('./path');
-const { generateText, checkAndGenerate } = require('./util');
+
+const { generateText, checkAndGenerate } = require("./util");
 
 // import is not a syntax natively supported by jest
 // import {generateText} from  './util';
 
 //globally available function provided by jest
 //Unit test
-test('should output name and age', ()=>{
-    const text = generateText('Song', 88);
-    expect (text).toBe('Song (88 years old)');
+test("should output name and age", () => {
+  const text = generateText("Song", 88);
+  expect(text).toBe("Song (88 years old)");
 });
 
 //Integration test
-test('should generate a valid text output', () => {
-    const text = checkAndGenerate('Yoda',1004);
-    expect(text).toBe('Yoda (1004 years old)')
+test("should generate a valid text output", () => {
+  const text = checkAndGenerate("Yoda", 1004);
+  expect(text).toBe("Yoda (1004 years old)");
 });
 
-test('should create an element with text and correct class', async () => {
-    const browser = await puppeteer.launch({
-        headless: true,
+test("should create an element with text and correct class", async () => {
+  const browser = await puppeteer.launch({
+    headless: true
+    // slowMo: 80,
+    // args: ['--window-size=1920,1080']
+  });
 
-    });
+  const page = await browser.newPage();
+  await page.goto(path);
+  await page.click("input#name");
+  await page.type("input#name", "SuperTrump");
+  await page.click("input#age");
+  await page.type("input#age", "28");
+  await page.click("#btnAddUser");
 
-    const page = await browser.newPage();
-    await page.goto(path);
-    await page.click('input#name');
-    await page.type('input#name', 'SuperTrump');
-    await page.click('input#age');
-    await page.type('input#age', '28');
-    await page.click('#btnAddUser');
-    
-    //eval => get access to element
-    const finalText = await page.$eval('.user-item', el => el.textContent);
-    expect(finalText).toBe('SuperTrump (28 years old)');
+  //eval => get access to element
+  const finalText = await page.$eval(".user-item", el => el.textContent);
+  expect(finalText).toBe("SuperTrump (28 years old)");
 }, 10000);
